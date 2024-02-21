1c06a30ef49c6529f918c8e977de62f799ca9f01
raheemadamboev <vividricemaryandmax@gmail.com>
2024-02-21 19:26:14 +0500

    Implemented dynamic shortcut;

--- app/src/main/java/xyz/teamgravity/appshortcutdemo/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/appshortcutdemo/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,6 +16,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
+import androidx.core.graphics.drawable.IconCompat
 import xyz.teamgravity.appshortcutdemo.ui.theme.AppShortcutDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,14 +55,41 @@ class MainActivity : ComponentActivity() {
                                 }
                             )
                         )
+                        Button(
+                            onClick = ::createDynamicShortcut
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.create_dynamic_shortcut)
+                            )
+                        }
                     }
                 }
             }
         }
     }
 
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        checkShortcutId(intent)
+    }
+
     private fun checkShortcutId(intent: Intent?) {
         val shortCutId = intent?.getStringExtra(KEY_SHORTCUT_ID) ?: return
         viewmodel.onShortcutChange(Shortcut.fromId(shortCutId))
+    }
+
+    private fun createDynamicShortcut() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.action = Intent.ACTION_VIEW
+        intent.putExtra(KEY_SHORTCUT_ID, Shortcut.Dynamic.id)
+
+        val shortcut = ShortcutInfoCompat.Builder(applicationContext, Shortcut.Dynamic.id)
+            .setShortLabel(getString(R.string.dynamic_short_label))
+            .setLongLabel(getString(R.string.dynamic_long_label))
+            .setDisabledMessage(getString(R.string.dynamic_disabled_label))
+            .setIcon(IconCompat.createWithResource(applicationContext, R.drawable.ic_adb))
+            .setIntent(intent)
+            .build()
+        ShortcutManagerCompat.pushDynamicShortcut(applicationContext, shortcut)
     }
 }
